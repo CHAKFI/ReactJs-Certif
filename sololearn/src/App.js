52766c11a0952506da2b63a0c50094bf8c4ba7cd
CHAKFI Ahmed <ahmedchakfi1999@gmail.com>
2023-02-14 09:33:45 +0000

    nothing int

--- sololearn/src/App.js
+++ sololearn/src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
 
+
+let counter = 0;
+
 function App() {
+  counter++;
+
   return (
     <div className="App">
       <header className="Appi-header" >
+
+        <h1> www.Scratchi.com </h1>
         <p>
           Hi im Scratchi, I just started coding with ReactJs...
         </p>
+        
+        <p>Count: {counter}</p>
 
       </header>
     </div>
