aa83afcdc0bc27965b67e4ddc0f5afd92d54430d
CHAKFI Ahmed <ahmedchakfi1999@gmail.com>
2023-02-14 10:16:09 +0000

    nothing int

--- sololearn/src/App.js
+++ sololearn/src/App.js
@@ -8,16 +8,23 @@ function App() {
 
   return (
     <div className="App">
-      <header className="Appi-header" >
+      <header className="App-header" >
 
         <h1> www.Scratchi.com </h1>
         <p>
           Hi im Scratchi, I just started coding with ReactJs...
         </p>
         
-        <p>Count: {counter}</p>
+        <p>ReactDOM.render function call Counter: {counter}</p>
 
       </header>
+
+    <footer>
+    
+    <p> The End </p>
+
+    </footer>
+      
     </div>
   );
 }
