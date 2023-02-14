afa6ff8843b93436911df87b11d61b86140d1d6e
CHAKFI Ahmed <ahmedchakfi1999@gmail.com>
2023-02-14 16:06:38 +0000

    Preparing for function att test

--- src/App.js
+++ src/App.js
@@ -18,23 +18,29 @@ function App() {
 
   return (
     <div className="App">
+              <div> 
+          
+          <ul>
+           <li><a href="default.asp">Home</a></li>
+           <li><a href="news.asp">News</a></li>
+           <li><a href="contact.asp">Contact</a></li>
+           <li><a href="about.asp">About</a></li>
+          </ul>
+         </div>
       <header className="App-header" >
 
         <h1> www.Scratchi.com </h1>
-        <p>
+        <p id="hi">
           Hi im Scratchi, I just started coding with ReactJs...
         </p>
-        
+        <p id="mssg">{<Info />} {counter}</p>
 
-        <p>{<Info />} {counter}</p>
 
       </header>
 
-    <footer>
-    
-    <p> The End </p>
-
-    </footer>
+      <footer>
+        <p id="end"> The End </p>
+      </footer>
       
     </div>
   );
