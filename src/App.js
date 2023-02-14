f87ab29716fa579e48c017bfda54889c03a909cd
CHAKFI Ahmed <ahmedchakfi1999@gmail.com>
2023-02-14 16:15:49 +0000

    function att test

--- src/App.js
+++ src/App.js
@@ -13,6 +13,10 @@ class Info extends React.Component{
   }
 }
 
+function AttTest(a){
+  return a;
+}
+
 function App() {
   counter++;
 
@@ -22,7 +26,7 @@ function App() {
           
           <ul>
            <li><a href="default.asp">Home</a></li>
-           <li><a href="news.asp">News</a></li>
+           <li><a href="news.asp">{AttTest("Day News")}</a></li>
            <li><a href="contact.asp">Contact</a></li>
            <li><a href="about.asp">About</a></li>
           </ul>
