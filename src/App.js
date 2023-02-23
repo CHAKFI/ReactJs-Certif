eaa4f7d7e04fe3ec911da8f69c4f5a225f58e956
CHAKFI Ahmed <ahmedchakfi1999@gmail.com>
2023-02-23 13:09:16 +0000

    Completing Form

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
           <div>
             <Converter />
           </div>
-          
+                
           <div>
           <p id="hi">
           <Form />
--- src/Form.js
+++ src/Form.js
@@ -1,11 +1,37 @@
+import React, {useState} from 'react';
 
 
 export default function Form (){
 
+    const [name, setName] = useState("there");
+    const [age, setAge] = useState("cool");
+    const [city, setCity] = useState("somewhere");
+
+    function handleChangeName(e) {
+        setName(e.target.value);
+    }
+
+    function handleChangeAge(e) {
+        setAge(e.target.value);
+    }
+
+    function handleChangeCity(e) {
+        setCity(e.target.value);
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+    }
+
     return (
         <div>
+            <form onSubmit={handleSubmit} >
+                Name: <input type="text" value={name} onChange={handleChangeName} /><br/>
+                Age: <input type="text" value={age} onChange={handleChangeAge} /><br/>
+                City: <input type="text" value={city} onChange={handleChangeCity} /><br/>
             
+                <p>Hi {"'"+name+"'"}, you are {"'"+age+"'"} and live in {"'"+city+"'"} </p>
+            </form>
         </div>
     );
-
 }
